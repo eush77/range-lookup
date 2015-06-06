be2979c1683308fcaf16d34f60598123a051e210
Eugene Sharygin <eush77@gmail.com>
2015-06-06 18:35:43 +0300

    Init gh-pages

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var rangeLookup = require('../');
+
+
+var lookup = function (query) {
+  var selection = window.getSelection();
+  selection.removeAllRanges();
+  rangeLookup(query).forEach(selection.addRange.bind(selection));
+};
+
+
+(function () {
+  var queryEl = document.querySelector('input');
+  queryEl.addEventListener('keyup', function () {
+    lookup(queryEl.value);
+  });
+}());
